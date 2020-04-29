3398bd27d28b37fbc1fe1abed80f0c7d86232433
Camilo <iecam03@gmail.com>
2020-04-28 19:54:21 -0500

    1

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const form = app.use(express.static(__dirname + '/public'))
+
+app.use(express.urlencoded({extended:true}));
+
+
+app.get('/', (req, res)=>{
+  res.send(form)
+});
+
+app.post('/forms/enviar/',(req,res)=>{
+  var nombre = req.body.nombre
+  console.log(req.body)
+  res.send("<h1><strong>Hola " + nombre + '!</strong><h1>')
+})
+
+app.listen(3000, () => {
+    return console.log('Listening on port 3000!');
+  });
+
